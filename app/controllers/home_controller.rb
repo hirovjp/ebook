36f2cd280eff9a4ce7b139decc54d3cc47519523
truongdubai <anhtruongdubai@gmail.com>
2019-11-11 23:29:31 +0700

    feature/learnwordlist: create function add wordlist learned

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
-  skip_before_action :authorize
+  skip_before_action :authorize, only: :index
 
   def home
-    @wordlist = Wordlist.all.paginate(:page => params[:page], :per_page => 10)
+    @wordlist = Wordlist.all.paginate(:page => params[:page], :per_page => 5)
     @user = User.find(session[:user_id])
     @demo = Wordlist.joins(:learnwordlists).where(:learnwordlists => {:user_id => @user.id})
   end
