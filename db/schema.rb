27de9fa924f4d6561cbcb5efe18a3b95b1194f02
truongdubai <anhtruongdubai@gmail.com>
2019-11-12 15:21:56 +0700

    feature/result: edit database

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_035414) do
+ActiveRecord::Schema.define(version: 2019_11_12_074959) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "fbs", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_fbs_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_fbs_on_reset_password_token", unique: true
   end
 
   create_table "learnwordlists", force: :cascade do |t|
@@ -27,8 +39,6 @@ ActiveRecord::Schema.define(version: 2019_11_08_035414) do
 
   create_table "lessons", force: :cascade do |t|
     t.string "title"
-    t.string "question"
-    t.string "answer"
     t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -57,6 +67,7 @@ ActiveRecord::Schema.define(version: 2019_11_08_035414) do
     t.string "translate_language"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "lesson_id"
   end
 
 end
